84d6c1e2281c18b3c3b0a15e9f7b60e4ed6fb98f
Raghavyadav1238 <raghavyadav999756@gmail.com>
2025-08-13 09:24:04 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello Raghav Yadav");
+        System.out.println("Btech");
+        System.out.println("2415001238");
+    }
+
+    
+}
