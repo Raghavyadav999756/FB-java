21bbcb030c2f120c395c6e425d0c6ba3a554c577
Raghavyadav1238 <raghavyadav999756@gmail.com>
2025-08-13 14:13:46 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    static int j=2;// static variable
+    public static void main(String[] args) {
+        int a=10;//local variable
+        System.out.println(a);
+        System.out.println(Variable.j);
+      
+
+    }
+    
+}
