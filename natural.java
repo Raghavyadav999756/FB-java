a9fe6e3e55fc391b48552492b88a80ac6e5bf763
Raghav yadav <raghavyadav999756@gmail.com>
2025-08-25 08:46:23 +0530

    natural number   1 to 5 by fir loop

---
+++ natural.java
@@ -0,0 +1,10 @@
+public class natural {
+    public static void main(String[] args) {
+        int n = 5; // You can change this value to generate more or fewer natural numbers
+        System.out.println("The first " + n + " natural numbers are:");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " ");
+        }
+    }
+    
+}
