a67d6f6500f0f9ea21d5696cd03220f4ce728ab4
Raghav yadav <raghavyadav999756@gmail.com>
2025-08-27 13:11:43 +0530

    declaration of array and uses

---
+++ array.java
@@ -0,0 +1,20 @@
+import java.util.Arrays ;
+public class array {
+    public static void main(String[] args) {
+        // Declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print the original array
+        System.out.println("Original Array: " + Arrays.toString(numbers));
+
+        // Modify an element in the array
+        numbers[2] = 100;
+
+        // Print the modified array
+        System.out.println("Modified Array: " + Arrays.toString(numbers));
+    
+
+        
+    }
+    
+}
