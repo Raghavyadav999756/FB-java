fab2d1a2719623916e0ac1e9a5ca909ddba5235b
Raghav yadav <raghavyadav999756@gmail.com>
2025-08-28 09:19:01 +0530

    reverse array in java

---
+++ reversearray.java
@@ -0,0 +1,10 @@
+public class reversearray {
+    public static void main(String[] args) {
+        int[] marks={1,2,3,4,5};
+        for(int i=marks.length-1;i>=0;i--){
+            System.out.print(marks[i] + " " );
+        }
+        
+    }
+    
+}
